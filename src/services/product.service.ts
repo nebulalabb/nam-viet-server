7ec70e35c3d17e58010193f70bc2b7ac5f106157
nhan27122003 <nhan27122003@gmail.com>
2026-03-06 17:19:52 +0700

    fix: sản phẩm, fix: kho, fix: nhập kho, xuất kho

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -127,6 +127,13 @@ class ProductService {
           select: {
             quantity: true,
             reservedQuantity: true,
+            warehouseId: true,
+            warehouse: {
+              select: {
+                id: true,
+                warehouseName: true,
+              }
+            }
           },
         },
         creator: {
